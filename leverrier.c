5cb54b47c58b4479fcd66f51117bf5d16fdcafbe
Unknown <corentinjeup@gmail.com>
2016-12-03 16:35:08 +0100

    ok

--- leverrier.c
+++ leverrier.c
@@ -1,23 +1,25 @@
-int traceMatrice(matrice*A,int n)
+#include "matrice.h"
+
+int traceMatrice(Matrice*A,int n)
 {
     int i, trace = 0;
     for (i=0; i<n; i++)
     {
-        trace += (*A)[i][i];
+        trace += A->matrice[i][i];
     }
     return trace;
 }
-int Leverrier(matrice*A, int n)
+int Leverrier(Matrice*A, int n)
 {
-    matrice*B NULL;
-    creerMatrice(*B,n,n);
+    Matrice*B = NULL;
+    creerMatrice(B,n,n);
     int tab[2][n];
     int i;
     int k;
     int resultat;
-    for(i=0;i<n:i++)
+    for(i=0;i<n;i++)
     {
-        puissance(&A, &B, n);
+        puissanceMatrice(&A, &B, n);
         tab[1][i] = traceMatrice(&B,n);
         tab[2][i] = tab[1][i];
         for(k=0; k<i-1; k++)
