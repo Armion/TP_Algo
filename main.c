7ee41cefacff924075d91f3e07cea80593e5c9c6
Armion <zarmion@gmail.com>
2016-12-03 13:16:13 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+/**
+ * \file main.c
+ * \brief contient la fonction main et la fonction erreure pour tester la lib
+ * \author Cochard Mathieu
+ * \version 1.5
+ * \date 21 Octobre 2016
+ *
+ */
+
+
+
+
+#include "matrice.h"
+#include "echantillon.h"
+#include "systeme_directe.h"
+#include "iterative.h"
+#include "test.h"
+
+
+
+
+int main()
+{
+    Matrice A, B, X;
+    int a = 0 ;
+    srand(time(NULL));
+
+
+
+    chargerMatrice(&A, "A");
+    chargerMatrice(&B, "B");
+    gauss(&A, &B, &X);
+    afficherMatrice(&X);
+
+
+
+
+
+
+
+
+    libererMatrice(&A);
+    libererMatrice(&B);
+    libererMatrice(&X);
+
+    scanf("%d", &a);
+
+    return 0;
+}
+
+
+
